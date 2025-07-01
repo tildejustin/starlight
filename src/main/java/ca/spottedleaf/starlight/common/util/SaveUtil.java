f2705d0a620cd77338d361f2f5a26e94fb101608
tildejustin <spacepepper3.14@gmail.com>
2025-07-01 14:03:53 -0400

    remove threaddeath reference, as it's deprecated for removal none of our mods nor vanilla use it anyway

--- src/main/java/ca/spottedleaf/starlight/common/util/SaveUtil.java
+++ src/main/java/ca/spottedleaf/starlight/common/util/SaveUtil.java
@@ -33,9 +33,6 @@ public final class SaveUtil {
         } catch (final Throwable ex) {
             // failing to inject is not fatal so we catch anything here. if it fails, it will have correctly set lit to false
             // for Vanilla to relight on load and it will not set our lit tag so we will relight on load
-            if (ex instanceof ThreadDeath) {
-                throw (ThreadDeath)ex;
-            }
             LOGGER.warn("Failed to inject light data into save data for chunk " + chunk.getPos() + ", chunk light will be recalculated on its next load", ex);
         }
     }
@@ -131,9 +128,6 @@ public final class SaveUtil {
         } catch (final Throwable ex) {
             // failing to inject is not fatal so we catch anything here. if it fails, then we simply relight. Not a problem, we get correct
             // lighting in both cases.
-            if (ex instanceof ThreadDeath) {
-                throw (ThreadDeath)ex;
-            }
             LOGGER.warn("Failed to load light for chunk " + pos + ", light will be recalculated", ex);
         }
     }
